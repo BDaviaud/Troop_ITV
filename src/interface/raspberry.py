9fec62a2a9549fda0858a4fc7238a786f4b56c0a
BDaviaud <berda@etud.univ-angers.fr>
2021-04-06 15:26:00 +0200

    Interpretation of a player though the raspberry configuration interface.

--- src/interface/raspberry.py
+++ src/interface/raspberry.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+from ..message import *
+
 try:
     from Tkinter import *
 except ImportError:
@@ -11,18 +13,14 @@ Widget that assigns raspberry input.
 
 class Raspberry():
     def __init__(self, master):
+        self.val = 0
+
         self.master = master
         self.root = Toplevel(master.root) # Popup -> Toplevel()
         self.root.geometry('300x300')
         self.root.title('Configuration Raspberry')
         self.root.grab_set() # Interaction with main window impossible
-        """
-        etiquette = Label(self.root, text='Entry :')
-        etiquette.pack(padx=5, pady=5, side=LEFT)
-
-        button = Button(self.root, text='Quitter', command=self.root.destroy).pack(padx=10, pady=10)
-        button.pack(padx=5, pady=5)
-        """
+        
         # Top : Raspberry Input
         frame_top = LabelFrame(self.root, text="Input", padx=20, pady=20)
         frame_top.pack(fill="both")
@@ -45,15 +43,25 @@ class Raspberry():
         frame_bottom = LabelFrame(self.root, text="Player", padx=20, pady=20)
         frame_bottom.pack(fill="both")
 
-        label3 = Label(frame_bottom, text='Set up player :')
+        label3 = Label(frame_bottom, text='Write player :')
         label3.pack(padx=5, pady=5, side=LEFT)
+
+        self.entry_player = Entry(frame_bottom, width=50)
+        self.entry_player.pack(padx=5, pady=5, side=LEFT)
+        self.entry_player.focus_force()
+
+        # Buttons
 
         btnCancel = Button(self.root, text='Cancel', command=self.root.destroy)
         btnCancel.pack(padx=5, pady=5, side=LEFT)
 
-        btnApply = Button(self.root, text='Apply')
+        btnApply = Button(self.root, text='Apply', command=self.apply_new_configuration)
         btnApply.pack(padx=5, pady=5, side=RIGHT)
         
-    
-        
+    def apply_new_configuration(self):
+        message = MSG_EVALUATE_STRING(self.master.text.marker.id , self.entry_player.get())
+        print(message)
+        self.master.add_to_send_queue(message)
+        self.root.destroy()
+
 
