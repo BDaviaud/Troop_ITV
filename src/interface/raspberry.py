8fa65d4aa7abf0392b144c2c4f97dd0d0ac6c641
BDaviaud <berda@etud.univ-angers.fr>
2021-04-20 10:25:36 +0200

    Modification of the Rasp Interface.

--- src/interface/raspberry.py
+++ src/interface/raspberry.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from ..message import *
+import random
 
 try:
     from Tkinter import *
@@ -14,12 +15,10 @@ Widget that assigns raspberry input.
 
 class Raspberry():
     def __init__(self, master):
-        self.val = self.current_val = 0 #Simulation de la mesure d'un capteur
-
         self.master = master
         self.root = Toplevel(master.root) # Popup -> Toplevel()
         self.root.title('New Player')
-        self.root.geometry('350x300')
+        self.root.geometry('350x250')
         self.root.grab_set() # Interaction with main window impossible
         """
         label = Label(frame_top, text='Select :')
@@ -37,71 +36,65 @@ class Raspberry():
         #frame_bottom.pack(fill="both")
 
         ## Player Name
-        Label(self.root, text='Enter a player name :').place(x=10, y=10)
+        Label(self.root, text='Enter the player name :').place(x=10, y=10)
         self.entry_player_name = Entry(self.root)
         self.entry_player_name.place(x=160, y=10)
 
-        # Player 
-        """
-        Label(self.root, text='Write the player :').place(x=10, y=50)
-        self.entry_player = Entry(self.root)
-        self.entry_player.place(x=10, y=70)
-        """
-
-        Label(self.root, text='Choose a synth:').place(x=10, y=50)
-        listSynth=["play", "pads","pluck","jbass", "loop"]
-        self.ComboSynth = ttk.Combobox(self.root, values=listSynth)
-        self.ComboSynth.current(0)
-        self.ComboSynth.place(x=160, y=50)
-
         # Param
-        Label(self.root, text='Choose a attribute:').place(x=10, y=90)
+        Label(self.root, text='Choose a attribute:').place(x=10, y=50)
         listParam=["degree", "playstring", "oct","dur","delay", "blur", "amplify", "scale", "bpm", "sample", "sus", "fmod", "pan", "rate", "amp", "vib", "vibdepth", "slide", "sus", "slidedelay", "slidefrom", "bend", "benddelay", "coarse", "striate", "pshift", "hpf", "hpr", "lpf", "lpr", "swell", "bpf", "bits", "crush", "dist", "chop", "tremolo", "echo", "decay", "spin", "cut", "room", "mix", "formant", "shape"]
         self.ComboParam = ttk.Combobox(self.root, values=listParam)
         self.ComboParam.current(0)
-        self.ComboParam.place(x=160, y=90)
+        self.ComboParam.place(x=160, y=50)
 
-        Label(self.root, text='Value:').place(x=10, y=120)
-        self.EntryValue = Entry(self.root)
-        self.EntryValue.place(x=160, y=120)
+        Label(self.root, text='Max value:').place(x=10, y=90)
+        self.EntryMaxValue = Entry(self.root)
+        self.EntryMaxValue.place(x=160, y=90)
+
+        Label(self.root, text='Min value:').place(x=10, y=130)
+        self.EntryMinValue = Entry(self.root)
+        self.EntryMinValue.place(x=160, y=130)
+
+        Label(self.root, text='Refresh Time (in second):').place(x=10, y=170)
+        self.SpinboxTime = Spinbox(self.root, from_=10, to=120)
+        self.SpinboxTime.place(x=160, y=170)
         
         # Buttons
 
         btnCancel = Button(self.root, text='Cancel', command=self.root.destroy)
-        btnCancel.place(x=10, y=190)
+        btnCancel.place(x=10, y=210)
 
         btnApply = Button(self.root, text='Apply', command=self.apply_new_configuration)
-        btnApply.place(x=270, y=190)
+        btnApply.place(x=270, y=210)
 
         
         
     def apply_new_configuration(self):
         self.player_name = self.entry_player_name.get()
-
-        self.synth = self.ComboSynth.get()
-
         self.param = self.ComboParam.get()
- 
-        self.value = self.EntryValue.get()
+        self.max = float(self.EntryMaxValue.get())
+        self.min = float(self.EntryMinValue.get())
+        self.refreshTime = int(self.SpinboxTime.get())
 
-        #SynthDef
-        message = self.player_name + " >> " + self.synth + "()"
-        message_server = MSG_EVALUATE_STRING(self.master.text.marker.id , message)
-        self.master.add_to_send_queue(message_server)
+        val = int(self.min + random.random() * (self.max - self.min))
+        print(val)
 
         #Param
-        message = self.player_name + "." + self.param + " = " + self.value 
+        message = self.player_name + "." + self.param + " = " + str(val) 
         message_server = MSG_EVALUATE_STRING(self.master.text.marker.id , message)
         self.master.add_to_send_queue(message_server)
 
-        #self.update_player() 
+        self.update_player() 
         self.root.destroy() # Close the popup
     
     def update_player(self):
-        self.current_val += 1
-        if self.current_val - self.val >= int(self.val_update):
-            self.val = self.current_val
-            print(self.val)
-        self.master.root.after(200, self.update_player)
+        val = int(self.min + random.random() * (self.max - self.min))
+        print(val)
+        
+        message = self.player_name + "." + self.param + " = " + str(val)
+        message_server = MSG_EVALUATE_STRING(self.master.text.marker.id , message)
+        self.master.add_to_send_queue(message_server)
+        
+        self.master.root.after(self.refreshTime*100, self.update_player)
 
 
