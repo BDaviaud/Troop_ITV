61231eea6725067541f010f9f61ff414eb393366
BDaviaud <daviaud.berangere@gmail.com>
2021-05-31 16:31:51 +0200

    Add GPIO to SensorInteraction.

--- src/interface/interaction.py
+++ src/interface/interaction.py
@@ -4,6 +4,7 @@ from ..message import *
 import random
 import re
 from FoxDot import *
+import RPi.GPIO as GPIO
 
 try:
     from Tkinter import *
@@ -18,12 +19,12 @@ Widget for raspberry input.
 class SensorInteraction():
     def __init__(self, master):
         self.master = master
-        self.root = Toplevel(master.root) # Popup -> Toplevel()
-        self.root.title('Rasp Config')
-        self.root.geometry('350x250')
-        self.root.grab_set() # Interaction with main window impossible
+        self.root = Toplevel(master.root) # Popup
+        self.root.title('Sensor and Player Configuration')
+        self.root.geometry('360x170')
+        self.root.grab_set() # Empêche d'interagir avec la fenêtre principale
         
-        # List of players playing         
+        # Liste des players en cours d'execution        
         pattern = re.compile(r'(?<=<)\w+')
         playing = self.master.lang.evaluate2("print('re', Clock.playing)")
 
@@ -32,51 +33,45 @@ class SensorInteraction():
         if not listPlayer:
             listPlayer = ['None']
 
-        ## Player Name
+        # Nom du player
         Label(self.root, text='Chose a player :').place(x=10, y=10)
         self.ComboPlayername = ttk.Combobox(self.root, values=listPlayer)
         self.ComboPlayername.current(0)
         self.ComboPlayername.place(x=160, y=10)
 
-        # Param
+        # Paramêtre
         Label(self.root, text='Choose a attribute:').place(x=10, y=50)
         listParam=["dur","degree", "playstring", "oct","delay", "blur", "amplify", "scale", "bpm", "sample", "sus", "fmod", "pan", "rate", "amp", "vib", "vibdepth", "slide", "sus", "slidedelay", "slidefrom", "bend", "benddelay", "coarse", "striate", "pshift", "hpf", "hpr", "lpf", "lpr", "swell", "bpf", "bits", "crush", "dist", "chop", "tremolo", "echo", "decay", "spin", "cut", "room", "mix", "formant", "shape"]
         self.ComboParam = ttk.Combobox(self.root, values=listParam)
         self.ComboParam.current(0)
         self.ComboParam.place(x=160, y=50)
-
-        Label(self.root, text='Max value:').place(x=10, y=90)
-        self.EntryMaxValue = Entry(self.root)
-        self.EntryMaxValue.place(x=160, y=90)
-
-        Label(self.root, text='Min value:').place(x=10, y=130)
-        self.EntryMinValue = Entry(self.root)
-        self.EntryMinValue.place(x=160, y=130)
-
-        Label(self.root, text='Refresh Time (in second):').place(x=10, y=170)
-        self.SpinboxTime = Spinbox(self.root, from_=10, to=120)
-        self.SpinboxTime.place(x=160, y=170)
+        
+        # Numéro de pin pour l'entrée GPIO
+        Label(self.root, text='Input pin GPIO :').place(x=10, y=90)
+        self.SpinboxGPIO = Spinbox(self.root, from_=1, to=40)
+        self.SpinboxGPIO.place(x=160, y=90)
         
         # Buttons
 
         btnCancel = Button(self.root, text='Cancel', command=self.root.destroy)
-        btnCancel.place(x=10, y=210)
+        btnCancel.place(x=10, y=130)
 
         btnApply = Button(self.root, text='Apply', command=self.apply_new_configuration)
-        btnApply.place(x=270, y=210)
+        btnApply.place(x=270, y=130)
         
     def apply_new_configuration(self):
         self.player_name = self.ComboPlayername.get()
         
         if self.player_name != 'None':
             self.param = self.ComboParam.get()
-            self.max = float(self.EntryMaxValue.get())
-            self.min = float(self.EntryMinValue.get())
-            self.refreshTime = int(self.SpinboxTime.get())
+            self.GPIOid = int(self.SpinboxGPIO.get())
+            
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(self.GPIOid, GPIO.OUT)
 
-            val = int(self.min + random.random() * (self.max - self.min))
+            self.current_value = GPIO.input(self.GPIOid)
             #Param
-            message = self.player_name + "." + self.param + " = " + str(val) 
+            message = self.player_name + "." + self.param + " = " + str(self.current_value) 
             message_server = MSG_EVALUATE_STRING(self.master.text.marker.id , message)
             self.master.add_to_send_queue(message_server)
 
@@ -85,21 +80,21 @@ class SensorInteraction():
         self.root.destroy() # Close the popup
     
     def update_player(self):
+        
         # Récupératinon de la variable isplaying du player (La valeur est retournée sous forme d'un tableau de string)
         commande = 'print("re", ' + self.player_name + '.isplaying)'
         isplaying = self.master.lang.evaluate2(commande)
-        
-        # Nouvelle valeur (random)
-        val = int(self.min + random.random() * (self.max - self.min))
 
         if isplaying[0] == 're False':
             return
-        else:         
-            message = self.player_name + "." + self.param + " = " + str(val)
-            message_server = MSG_EVALUATE_STRING(self.master.text.marker.id , message)
-            self.master.add_to_send_queue(message_server)
+        else:
+            if GPIO.input(self.GPIOid) != self.current_value:
+                self.current_value = GPIO.input(self.GPIOid)
+                message = self.player_name + "." + self.param + " = " + str(self.current_value)
+                message_server = MSG_EVALUATE_STRING(self.master.text.marker.id , message)
+                self.master.add_to_send_queue(message_server)
             
-            self.master.root.after(self.refreshTime*100, self.update_player)
+            self.master.root.after(200, self.update_player)
         
 """
 Widget for orchestration.
